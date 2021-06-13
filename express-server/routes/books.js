429d6db289f18ff0cd00e167a248f04d720bf213
OArteiro <cyrne18@gmail.com>
2021-06-13 03:00:31 -0300

    feat(express-server): created express based back-end

---
+++ express-server/routes/books.js
@@ -0,0 +1,147 @@
+const router = require('express').Router()
+
+// define the default route that fetches all of our books
+router.get('/', async function (req, res) {
+
+    // data the conserves our API quota for development
+    const placeholderData = [
+        {
+            "_id": "database15911271768852",
+            "book": {
+                "_id": "database1594127768852",
+                "title": "Title 1",
+                "author": "Author 1",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T19:56:08.852Z",
+            "_lastModifiedOn": "2020-06-02T19:56:08.852Z"
+        },
+        {
+            "_id": "database1593134992139",
+            "book": {
+                "_id": "database1591126768852",
+                "title": "Title 2",
+                "author": "Author 2",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T21:56:32.139Z",
+            "_lastModifiedOn": "2020-06-02T21:56:32.139Z"
+        },
+        {
+            "_id": "database158112776812",
+            "book": {
+                "_id": "database1561127768852",
+                "title": "Title 3",
+                "author": "Author 3",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T19:56:08.852Z",
+            "_lastModifiedOn": "2020-06-02T19:56:08.852Z"
+        },
+        {
+            "_id": "database1591132092139",
+            "book": {
+                "_id": "database1591127268852",
+                "title": "Title 4",
+                "author": "Author 4",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T21:56:32.139Z",
+            "_lastModifiedOn": "2020-06-02T21:56:32.139Z"
+        },
+        {
+            "_id": "database1591127563852",
+            "book": {
+                "_id": "database1591126768852",
+                "title": "Title 5",
+                "author": "Author 5",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T19:56:08.852Z",
+            "_lastModifiedOn": "2020-06-02T19:56:08.852Z"
+        },
+        {
+            "_id": "database1591134996139",
+            "book": {
+                "_id": "database1581127768852",
+                "title": "Title 6",
+                "author": "Author 6",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T21:56:32.139Z",
+            "_lastModifiedOn": "2020-06-02T21:56:32.139Z"
+        },
+        {
+            "_id": "database1591227763852",
+            "book": {
+                "_id": "database1596127768852",
+                "title": "Title 7",
+                "author": "Author 7",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T19:56:08.852Z",
+            "_lastModifiedOn": "2020-06-02T19:56:08.852Z"
+        },
+        {
+            "_id": "database1591464992139",
+            "book": {
+                "_id": "database1591127768852",
+                "title": "Title 8",
+                "author": "Author 8",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T21:56:32.139Z",
+            "_lastModifiedOn": "2020-06-02T21:56:32.139Z"
+        },
+        {
+            "_id": "database1591117768852",
+            "book": [{
+                "_id": "database1591127778852",
+                "title": "Title 9",
+                "author": "Author 9",
+                "image": "",
+            }],
+            "_createdOn": "2020-06-02T19:56:08.852Z",
+            "_lastModifiedOn": "2020-06-02T19:56:08.852Z"
+        },
+        {
+            "_id": "database1591194992139",
+            "book": {
+                "_id": "database159111768852",
+                "title": "Title 10",
+                "author": "Author 10",
+                "image": "",
+            },
+            "_createdOn": "2020-06-02T21:56:32.139Z",
+            "_lastModifiedOn": "2020-06-02T21:56:32.139Z"
+        }
+    ]
+
+    try {
+        res.json(placeholderData)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send('Error.')
+    }
+})
+
+router.post('/add', async function (req, res) {
+    const { book } = req.body
+
+    const data = {
+        book
+    }
+
+    console.log(data)
+
+    try {
+        res.json({
+            message: 'Book added'
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Error.")
+    }
+})
+
+module.exports = router
---
+++ express-server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const booksRouter = require('./routes/books')
+
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Add headers
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(express.json());
+
+app.use('/', booksRouter)
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
