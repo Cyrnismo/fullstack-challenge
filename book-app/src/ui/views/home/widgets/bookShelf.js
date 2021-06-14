bcfc07141277f7572e43f64d62e80defa403e66c
OArteiro <cyrne18@gmail.com>
2021-06-14 03:48:18 -0300

    wip(bookShelf): suffering all weekend to deal with CORS

--- book-app/src/ui/views/home/widgets/bookShelf.js
+++ book-app/src/ui/views/home/widgets/bookShelf.js
@@ -1,28 +1,62 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import hooked from '../../../../assets/images/hooked.jpg';
 
 export const BookShelf = () => {
+    let [books, setBooks] = useState([]);
+    let [activeBook, setActiveBook] = useState('');
+
+
+    const getBooksWithAxios = useCallback(() => {
+        var _url = 'http://192.168.0.110:3001/api/books';
+
+        const axiosResponse = axios.get({ method: 'get', url: _url }).then(
+            async response => {
+                try {
+                const data = await response.json();
+                console.log('response data?', data);
+                // setBooks(data);
+                } catch (error) {
+                    console.log('Error Axios!');
+                    console.log(error);
+                }
+            }
+        );
+        return axiosResponse;
+    }, []);
+
+    const getBooks = useCallback(() => {
+        var url = 'http://192.168.0.110:3001/api/books';
+
+        fetch(url, { method: 'GET'} ).then(
+            async response => {
+                try {
+                    const data = await response.json();
+                    console.log('response data?', data)
+                    // setBooks(data);
+                } catch (error) {
+                    console.log('Error happened here!')
+                    console.log(error);
+                }
+            }
+        )
+    }, [])
+
+    /* useEffect(() => {
+        getBooksWithAxios()
+        getBooks()
+    }, [getBooksWithAxios, getBooks]);
+ */
     return (
-        //TODO: Implement auto fetch list view
         <BSContainer>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />  
+            {/* {books.map((book, key) => {
+                return (
+                    <Book key={key}  cover={book.image} title={book.title} author={book.author} description={book.description} onClick={() => {setActiveBook(book._id)}} />
+                );
+            })} */}
         </BSContainer>
     );
 }
@@ -75,6 +109,7 @@ const Image = styled.img`
     display: block;
     width: 105px;
     height: 153px;
+    box-shadow: 2px 2px 23px rgba(107, 103, 70, 0.125901);
 `;
 
 const BookTitle = ({title}) => {
