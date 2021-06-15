76ea75d544752f8b1e0aa431ead9ad4009ba1282
OArteiro <cyrne18@gmail.com>
2021-06-15 05:15:11 -0300

    feat(wip): connecting boolshelf to search bar method

--- book-app/src/ui/views/home/Home.js
+++ book-app/src/ui/views/home/Home.js
@@ -1,17 +1,77 @@
-import React from 'react';
 import '../../../App.css';
+import axios from 'axios';
+import Fuse from 'fuse.js';
 import styled from 'styled-components';
 import {SearchBar} from './widgets/searchBar';
-import { BookShelf } from './widgets/bookShelf';
 import { Navbar } from '../../commom_widgets/navbar';
 import { UserGreetings } from './widgets/userGreetings';
+import { BookShelf } from './widgets/bookShelf/bookShelf';
+import React, { useState, useEffect, useCallback } from 'react';
 
-export default function Home() {  
+export default function Home() {
+    let [books, setBooks] = useState([]);
+    let [data, setData] = useState([]);
+
+    const onKeyDownHandler = e => {
+      if (e.keyCode === 13) {
+          searchData(e);
+      }
+    };
+
+    const searchData = (pattern) => {
+      const matches = [];
+
+      if (!pattern) {
+        setData(books);
+        return;
+      }
+
+      const fuse = new Fuse(books.books, {
+        keys: ['title', 'author'],
+      });
+
+      const result = fuse.search(pattern);
+      if (!result.length) {
+        setData([]);
+      } else {
+        result.forEach(({item}) => {
+          matches.push(item);
+        });
+        setData(matches);
+      }
+    }
+
+    const getBooks = useCallback(() => {
+        var _url = 'http://localhost:3001/api/books';
+
+        fetch(_url, { method: 'GET'} ).then(
+            async response => {
+                try {
+                    const data = await response.json();
+                    console.log(data);
+                    setBooks(books);
+                    setData(data);
+                } catch (error) {
+                    console.log('OMG! Error happened here!')
+                    console.log(error);
+                }
+            }
+        )
+    }, [books])
+
+    useEffect(() => {
+        getBooks()
+    }, [getBooks])
+    
     return(
         <Background>
-            <SearchBar />
+            <SearchBar
+              placeholder={"Let's jump into a new adventure!"}
+              onChange={e => searchData(e.target.value)}
+              onKeyDownHandler={onKeyDownHandler}
+            />
             <UserGreetings />
-            <BookShelf />
+            <BookShelf books={books} data={data} />
             <Navbar />
         </Background>
     );
