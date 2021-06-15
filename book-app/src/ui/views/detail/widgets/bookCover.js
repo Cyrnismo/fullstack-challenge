9c85554ad63d85525360e34eb9b0e6c52787837f
OArteiro <cyrne18@gmail.com>
2021-06-15 05:14:21 -0300

    feat: book cover image now returning from api

--- book-app/src/ui/views/detail/widgets/bookCover.js
+++ book-app/src/ui/views/detail/widgets/bookCover.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import hooked from '../../../../assets/images/hooked.jpg';
 
-export const BookCover = () => {
+export const BookCover = ({cover}) => {
     return (
         <>
-            <BCImage src={hooked} />
+            <BCImage src={cover} />
         </>
     );
 }
