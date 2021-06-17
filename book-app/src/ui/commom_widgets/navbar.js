5645384bc27f825f79f0c628023402955b565ab0
OArteiro <cyrne18@gmail.com>
2021-06-17 05:17:13 -0300

    wip(navbar): suffering to maintain state

--- book-app/src/ui/commom_widgets/navbar.js
+++ book-app/src/ui/commom_widgets/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Person } from '@styled-icons/bootstrap/Person';
@@ -6,37 +6,11 @@ import { Add } from '@styled-icons/fluentui-system-filled/Add';
 import { HomeAlt } from '@styled-icons/boxicons-regular/HomeAlt';
 
 export const Navbar = () => {
-    let [isHome, setHome] = useState(false);
-    let [isAdd, setAdd] = useState(false);
-    let [isPerson, setPerson] = useState(false);
-
-    function onClickHome() {
-        setHome(true);
-        setAdd(false);
-        setPerson(false);
-    }
-
-    function onClickAdd() {
-        setAdd(true);
-        setHome(false);
-        setPerson(false);
-    }
-
-    function onClickPerson() {
-        setPerson(true);
-        setHome(false);
-        setAdd(false);
-    }
-
-    /* useEffect(() => {
-        console.log(isHome, isAdd, isPerson);
-    }, [isHome, isAdd, isPerson]) */
-
     return(
         <NContainer>
-            <HomeBtn isHome={isHome} onClick={() => onClickHome()} />
-            <AddBtn isAdd={isAdd} onClick={() => onClickAdd()} />
-            <PersonBtn isPerson={isPerson} onClick={() => onClickPerson()} />
+            <HomeBtn />
+            <AddBtn />
+            <PersonBtn />
         </NContainer>    
     );
 }
@@ -65,6 +39,7 @@ const BtnContainer = styled(Link)`
     width: 100px;
     height: 40px;
     text-decoration: none;
+    color: ${(props) => props.active === true ? '#000' : '#BFBEBF'}
 `;
 
 const BtnLabel = styled.h4`
@@ -76,17 +51,17 @@ const BtnLabel = styled.h4`
     font-size: 10px;
     line-height: 12px;
     color: #313131;
+    color: ${(props) => props.active === true ? '#000' : '#BFBEBF'}
+    background-color: 2px 2px 16px #000;
 `;
 
 // Buttons ~
 
-const HomeBtn = (isHome) => {
-    // console.log(`Home: ${isHome}`);
-
+const HomeBtn = ({isHome}) => {
     return (
-        <BtnContainer to='/'>
-            <HomeIcon size={24} isHome={isHome} />
-            <HomeLabel isHome={isHome}>Home</HomeLabel>
+        <BtnContainer to={'/'}>
+            <HomeIcon size={24} active={isHome} />
+            <HomeLabel active={isHome}>Home</HomeLabel>
         </BtnContainer>
     );
 }
@@ -94,21 +69,16 @@ const HomeBtn = (isHome) => {
 const HomeIcon = styled(HomeAlt)`
     position: absolute;
     top: 10px;
-    color: ${(props) => props.isHome === 'true' ? '#000' : '#BFBEBF'};
 `;
 
 const HomeLabel = styled(BtnLabel)`
-    color: ${(props) => props.isHome === 'true' ? '#000' : '#BFBEBF'};
-    background-color: 2px 2px 16px #000;
 `;
 
 const AddBtn = ({isAdd}) => {
-    // console.log(`Add: ${isAdd}`);
-
     return (
-        <BtnContainer to='/create'>
-            <AddIcon size={24} isAdd={isAdd} />
-            <AddLabel isAdd={isAdd}>Add</AddLabel>
+        <BtnContainer to={'/create'}>
+            <AddIcon size={24} active={isAdd} />
+            <AddLabel active={isAdd}>Add</AddLabel>
         </BtnContainer>
     );
 }
@@ -116,20 +86,15 @@ const AddBtn = ({isAdd}) => {
 const AddIcon = styled(Add)`
     position: absolute;
     top: 10px;
-    color: ${(props) => props.isAdd === 'true' ? '#000' : '#BFBEBF'};
 `;
 
-const AddLabel = styled(BtnLabel)`
-    color: ${(props) => props.isAdd === 'true' ? '#000' : '#BFBEBF'};
-`;
+const AddLabel = styled(BtnLabel)``;
 
 const PersonBtn = ({isPerson}) => {
-    // console.log(`Person: ${isPerson}`);
-
     return (
-        <BtnContainer to='/detail'>
-            <PersonIcon size={24} isPerson={isPerson} />
-            <PersonLabel isPerson={isPerson}>Person</PersonLabel>
+        <BtnContainer to={'/detail'}>
+            <PersonIcon size={24} />
+            <PersonLabel>Person</PersonLabel>
         </BtnContainer>
     );
 }
@@ -137,9 +102,6 @@ const PersonBtn = ({isPerson}) => {
 const PersonIcon = styled(Person)`
     position: absolute;
     top: 10px;
-    color: ${(props) => props.isPerson === 'true' ? '#000' : '#BFBEBF'};
 `;
 
-const PersonLabel = styled(BtnLabel)`
-    color: ${(props) => props.isPerson === 'true' ? '#000' : '#BFBEBF'};
-`;
+const PersonLabel = styled(BtnLabel)``;
