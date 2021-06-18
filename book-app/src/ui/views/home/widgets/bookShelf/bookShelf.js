51b715779f6a6f642d7470a4f1afadd0903089b3
OArteiro <cyrne18@gmail.com>
2021-06-18 03:57:56 -0300

    feat/style(bookShelf): tried to keep aim of last book

--- book-app/src/ui/views/home/widgets/bookShelf/bookShelf.js
+++ book-app/src/ui/views/home/widgets/bookShelf/bookShelf.js
@@ -1,29 +1,47 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
 import styled from 'styled-components';
 import { Book } from './widgets/book';
-import React from 'react';
 
-export const BookShelf = ({books}) => {
+export const BookShelf = ({books, lastBookElementRef}) => {
   return (
     <BSContainer>
       {books && books.map((book, index) => {
-        return (
-          <Book key={index} cover={book.image} title={book.title} author={book.author} />
-        );
+        if (books.length === index + 1) {
+          return (
+            <Book key={index} id={book._id} cover={book.image} title={book.title} author={book.author} innerRef={lastBookElementRef} />
+          );
+        } else {
+          return (
+            <Book key={index} id={book._id} cover={book.image} title={book.title} author={book.author} />
+          );
+        }
       })}
     </BSContainer>
   );
 }
 
 const BSContainer = styled.div`
-    padding: 0.25rem;
-    margin: 0.25rem;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    position: relative;
-    top: 21%;
-    width: 100%;
-    height: 563px;
-    overflow-x: auto;
+  padding: 0.25rem;
+  margin: 0.25rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  position: relative;
+  top: 21%;
+  left: -10px;
+  padding-top: 20px;
+  padding-left: 20px;
+  width: 94%;
+  height: 566px;
+  overflow-x: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  border-radius: 16px 16px 16px 0px;
+  box-shadow: inset 2.2px 2.2px 16px rgba(107, 103, 70, 0.1293);
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
